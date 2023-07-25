d6291e28d14c7489067fdfb456f7dcfa87e3b327
Yunusov <abubakar.yunusov.47@mail.ru>
2023-07-25 15:36:53 +0300

    Исправил Multer

--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -2,15 +2,15 @@ const multer = require("multer");
 
 // создание Multer для добавления картинок
 const storage = multer.diskStorage({
-	destination(_, _, cb) {
+	destination(req, res, cb) {
 		cb(null, "images");
 	},
-	filename(_, file, cb) {
+	filename(req, file, cb) {
 		cb(null, file.originalname);
 	},
 });
 const types = ["image/png", "image/jpeg", "image/jpg", "image/svg"];
-const fileFilter = (_, file, cb) => {
+const fileFilter = ( req, file, cb) => {
 	if (types.includes(file.mimetype)) {
 		cb(null, true);
 	} else {
